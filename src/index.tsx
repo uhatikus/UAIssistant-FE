6fb3dd61f324a83440fdfaa7907d5fb271308288
Ukhatov Alexander <alexander.ukhatov@gmail.com>
2024-03-31 00:35:07 +0100

    feat(skeleton): FE initialized

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello, React with TypeScript!!!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
